37394d9975d64a9a5056aff685712cad1b7cd629
nakamitsu_nozomi <exnnss0131@gmail.com>
2023-10-24 20:16:30 +0900

    feat: force-dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-
+export const dynamic = "force-dynamic";
 export default async function Home() {
 
   const getGroupByID = async() => {
